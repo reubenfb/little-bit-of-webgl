73f5178b758eff75a21d2a5e0a0e9d3adda1f25f
Reuben Fischer-Baum <reuben.fischerbaum@gmail.com>
2017-10-18 14:14:39 -0400

    update

--- src/js/app.js
+++ src/js/app.js
@@ -22,10 +22,10 @@ const convertScale = d3.scaleLinear().range([-1, 1]).domain([0, 1000]);
 for(var i = 0; i < totalLeg; i++){
 	let square = [];
 
-	let colPosition = (i % squaresPerRow) * squares.width + squares.width/2;
+	let colPosition = (i % squaresPerRow) * squares.width + squares.width/2 - 1;
 	square.push(convertScale(colPosition));
 
-	let rowPosition = Math.floor(i/squaresPerRow) * squares.width + squares.width/2;
+	let rowPosition = Math.floor(i/squaresPerRow) * squares.width + squares.width/2 - 1;
 	square.push(-convertScale(rowPosition));
 
 	squares.position.push(square);
@@ -44,13 +44,6 @@ for(var i = 0; i < totalLeg; i++){
 
 const drawSquares = regl({
 
-  frag: `
-  precision mediump float;
-  varying vec4 fragColor;
-  void main () {
-    gl_FragColor = fragColor;
-  }`,
-
   vert: `
   precision mediump float;
   attribute vec2 position;
@@ -58,9 +51,16 @@ const drawSquares = regl({
   varying vec4 fragColor;
   uniform float pointWidth;
   void main () {
-  	gl_PointSize = pointWidth;
+    gl_PointSize = pointWidth;
     gl_Position = vec4(position, 0, 1);
     fragColor = color;
+  }`,
+
+  frag: `
+  precision mediump float;
+  varying vec4 fragColor;
+  void main () {
+    gl_FragColor = fragColor;
   }`,
 
   attributes: {
@@ -73,7 +73,7 @@ const drawSquares = regl({
   },
 
   uniforms: {
-    pointWidth: squares.width - squares.margin * 2
+    pointWidth: squares.width * window.devicePixelRatio - squares.margin * window.devicePixelRatio * 2
   },
 
   count: totalLeg,
